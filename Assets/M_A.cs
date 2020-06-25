cd8119ba030f86890887039bb72be6bcdcddde21
Juan Manuel <juan_man.becerra@uao.edu.co>
2020-06-24 22:58:42 -0500

    ahora si primera version

---
+++ Assets/M_A.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO.Ports;
+
+public class M_A : MonoBehaviour {
+    SerialPort sp = new SerialPort("COM5", 9600);
+    int ON;
+    // Use this for initialization
+    void Start () {     
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (sp.IsOpen) {
+            try {
+
+                //string un = sp.ReadLine();
+                ON = sp.ReadByte();
+
+                if (ON == 10)
+                {
+                    movimiento(sp.ReadByte());
+                }
+                else {
+                    //se apaga el carro
+                }
+            }
+            catch (System.Exception) { }
+        }
+
+    }
+
+    void movimiento(int direccion) { 
+          
+        if (direccion == 1) {         
+            transform.Translate(Vector3.forward * 15f * Time.deltaTime);
+        }
+
+        if (direccion == 2) {       
+            transform.Translate(Vector3.forward * 25f * Time.deltaTime);
+        }
+
+        if (direccion == 3) {        
+            transform.Translate(Vector3.back * 15f * Time.deltaTime);
+        }
+
+        if (direccion == 4) {       
+            transform.Translate(Vector3.back * 0f * Time.deltaTime);
+        }
+
+        /* if (direccion == 2)
+         {
+             transform.Translate(Vector3.right * speed * Time.deltaTime);
+         }*/
+    }
+}
---
+++ Assets/M_A2S.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+
+public class M_A2S : MonoBehaviour
+{
+
+    SerialPort sp2 = new SerialPort("COM5", 9600);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp2.Open();
+        sp2.ReadTimeout = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sp2.IsOpen)
+        {
+            try
+            {
+                acelerar(sp2.ReadByte());
+                frenar(sp2.ReadByte());
+            }
+            catch (System.Exception) { }
+        }
+    }
+
+
+    void acelerar(int direccion)
+    {
+        if (direccion == 1)
+        {
+            transform.Translate(Vector3.forward * 15f * Time.deltaTime);
+        }
+
+        if (direccion == 3)
+        {
+            transform.Translate(Vector3.forward * 30f * Time.deltaTime);
+        }
+    }
+
+
+    void frenar(int direccion)
+    {
+        if (direccion == 2)
+        {
+            transform.Translate(Vector3.back * 30f * Time.deltaTime);
+        }
+
+        if (direccion == 4)
+        {
+            transform.Translate(Vector3.back * 0f * Time.deltaTime);
+        }
+    }
+}
+
